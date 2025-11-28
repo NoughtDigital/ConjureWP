5b9afb4a5bc36f2dc32faa5ba3d7ef4b019ad24a
Jake Henshall <jakehenshall93@gmail.com>
2025-11-28 22:14:56 +0000

    chore: Adding in doctum docs

---
+++ doctum.php
@@ -0,0 +1,39 @@
+<?php
+
+use Doctum\Doctum;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
+use Symfony\Component\Finder\Finder;
+
+$dir = __DIR__;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('vendor')
+    ->exclude('node_modules')
+    ->exclude('build')
+    ->exclude('dist')
+    ->exclude('demo')
+    ->exclude('examples')
+    ->exclude('scripts')
+    ->exclude('tests')
+    ->in($dir);
+
+return new Doctum($iterator, [
+    'title'                => 'ConjureWP API Documentation',
+    'language'             => 'en',
+    'build_dir'            => __DIR__ . '/docs-api/build',
+    'cache_dir'            => __DIR__ . '/docs-api/cache',
+    'source_dir'           => $dir . '/',
+    'remote_repository'    => new GitHubRemoteRepository('jakehenshall/ConjureWP', $dir),
+    'default_opened_level' => 2,
+    'footer_link'          => [
+        'href'        => 'https://github.com/code-lts/doctum',
+        'rel'         => 'noreferrer noopener',
+        'target'      => '_blank',
+        'before_text' => 'Documentation generated with',
+        'link_text'   => 'Doctum',
+        'after_text'  => '',
+    ],
+]);
+
---
+++ generate-api-docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Generate API documentation using Doctum
+# Run this script after making changes to PHP files
+
+echo "Generating ConjureWP API Documentation..."
+php vendor/bin/doctum.php update doctum.php -v
+
+if [ $? -eq 0 ]; then
+    echo ""
+    echo "Documentation generated successfully!"
+    echo "Output: docs-api/build/"
+    echo ""
+    echo "JSON API for Next.js: docs-api/build/doctum-search.json"
+    echo "HTML Documentation: docs-api/build/index.html"
+else
+    echo ""
+    echo "Documentation generation failed!"
+    exit 1
+fi
+
